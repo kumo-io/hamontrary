519ddf74e9870e40d93a800972d6c9fa7b567ca2
DeeChael <DeeChael@outlook.com>
2024-01-26 00:29:11 +0800

    feat: initialized

---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import {NextUIProvider} from '@nextui-org/react'
+import {AppProps} from "next/app";
+
+function App({ Component, pageProps }: AppProps) {
+    return (
+        <NextUIProvider>
+            <Component {...pageProps} />
+        </NextUIProvider>
+    )
+}
+
+export default App;
